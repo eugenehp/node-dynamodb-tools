0fb9221edc19a17d71dacb14f8579a10a2cb15ca
Eugene HP <eugene.hp2012@gmail.com>
2016-10-09 04:38:30 -0700

    almost there

---
+++ export_database.js
@@ -0,0 +1,56 @@
+var async = require('async');
+var status = require('node-status');
+var exportTable = require("./export_table");
+
+const PAGE_SIZE = 100;
+
+module.exports = function(dynamodb){
+
+  getTables(dynamodb, [], {}, function(err, tables){
+
+    var pattern = "  Exporting tables: {uptime}  |  {spinner.cyan}";
+    for( i in tables)
+      pattern += "  | "+tables[i]+"  {"+tables[i]+".green.bar}";
+
+    status.start({
+      // invert: true,
+      interval: 200,
+      pattern: pattern
+    });
+
+
+    async.mapSeries(tables, exportTableWrapper, function(err, results){
+      console.log('RESULTS', err, results);
+
+      setTimeout(function(){
+          status.stop();
+          console.log('\n');
+      }, 500);
+
+    });
+  });
+
+  function exportTableWrapper(tableName, cb){
+    exportTable(dynamodb, tableName, status, __dirname, cb);
+  }
+}
+
+
+
+function getTables(dynamodb, tables, params, cb){
+  params.Limit = PAGE_SIZE;
+
+  dynamodb.listTables(params, function(err, data) {
+    if (err) {
+      cb(err)
+    } else if (data.LastEvaluatedTableName){
+      params.ExclusiveStartTableName = data.LastEvaluatedTableName;
+
+      tables = tables.concat( data.TableNames );
+      getTables(dynamodb, tables, params, cb);
+    }else {
+      tables = tables.concat( data.TableNames );
+      cb( null, tables );
+    }
+  });
+}
--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 var AWS = require("aws-sdk");
-var status = require('node-status');
-var exportTable = require("./export_table");
+var exportDatabase = require("./export_database");
 
 AWS.config.loadFromPath('./config.export.json');
 
 var dynamodb = new AWS.DynamoDB();
 
-/*var params = {
-  Limit: 1
-};
+exportDatabase(dynamodb);
 
-dynamodb.listTables(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});*/
-
+/*
 var tableName = 'accesstokens';
 exportTable(dynamodb, tableName, status, __dirname, function(err, data){
   setTimeout(function(){
     status.stop();
     console.log('\n');
   }, 500);
-  
 });
+*/
 
-status.start({
-  // invert: true,
-  interval: 200,
-  pattern: "  Doing work: {uptime}  |  {spinner.cyan}  | "+tableName+"  {"+tableName+".green.bar}"
-});
